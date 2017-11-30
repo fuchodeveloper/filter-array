2a4233084d0898859bf987e1cc324c44dc56cf3b
Fredrick Mgbeoma <fuchodeveloper@Mgbeomas-MacBook-Pro.local>
2017-12-01 00:30:44 +0100

    chore(update-test): update existing tests for function

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import should from 'should';
 import filterArrayInput from '../src/filterArrayInput';
 
-describe('Simple test to illustrate testing and debugging', () => {
+describe('Test for user supplied input in filterArrayInput function', () => {
   it('should return "Please supply an input" if input is not supplied', (done) => {
     expect(filterArrayInput()).to.equal('Please supply an input');   
     done();
@@ -13,32 +13,46 @@ describe('Simple test to illustrate testing and debugging', () => {
     done();
   });
 
-  it('should contain an array of numbers', (done) => {
-    expect(filterArrayInput('hello world')).to.equal('Input must be an array of numbers');
+  it('should not be a string input', (done) => {
+    expect(filterArrayInput('hello world')).to.equal('Input must be an array');
     done();
-  })
+  });
 
-  it('should return number even if even and odd if odd ', (done) => {
-    expect(filterArrayInput([1, 2, 3, 4])).to.equal({evenArray:[2, 4], oddArray:[1, 3]});
+  it('should only accept an array of numbers', (done) => {
+    expect(filterArrayInput(2,2,2,2)).to.equal('Input must be an array of numbers');
     done();
-  })
+  });
 
-  it('should return number even if even and odd if odd ', (done) => {
-    expect(filterArrayInput(2,2,2,2)).to.equal('Input must be an array');
-    done();
-  })
-
-  it('should return number even if even and odd if odd ', (done) => {
+  it('should return "Please pass in an array" if object arguments are passed', (done) => {
     expect(filterArrayInput({})).to.equal('Please pass in an array');
     done();
-  })
+  });
 
-  it('should return even if it is ', (done) => {
-    const filteredOutput = [1, 2, 3, 4, 5, 6];
-    filteredOutput.should.be.an('object');
-    filteredOutput.should.have.a.property('evenArray');
-    filteredOutput.should.have.a.property('oddArray');
+});
+
+describe('Test for filterArrayInput results', () => {
+  it('should expect result to be an object', (done) => {
+    expect(filterArrayInput([1, 2, 3, 4, 5, 6])).to.be.an('object');
     done();
-  })
+  });
 
+  it('should expect result to have a property "evenArray"', (done) => {
+    expect(filterArrayInput([1, 2, 3, 4, 5, 6])).to.have.a.property('evenArray');
+    done();
+  });
+
+  it('should expect result to have a property "oddArray"', (done) => {
+    expect(filterArrayInput([1, 2, 3, 4, 5, 6])).to.have.a.property('oddArray');
+    done();
+  });
+
+  it('should expect oddArray in result to not be empty ', (done) => {
+    expect(filterArrayInput([1, 2, 3, 4, 5, 6])).to.have.a.property('oddArray').to.not.be.empty;
+    done();
+  });
+
+  it('should expect evenArray in result to not be empty', (done) => {
+    expect(filterArrayInput([1, 2, 3, 4, 5, 6])).to.have.a.property('evenArray').to.not.be.empty;
+    done();
+  });
 });
